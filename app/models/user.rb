d33e43cfec9fba02a67560117b122a1fbe2ddb5d
Daniel Homer <daniel.homer6@gmail.com>
2016-04-17 22:07:17 -0400

    Change grammar in round/show, fix math in user methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,24 @@ class User < ActiveRecord::Base
   validates :username, :email, presence: true, uniqueness: true
 
   def rounds_played
-    self.rounds.select{|round| round.complete?}.length
+    self.rounds.select{|round| round.complete?}
+  end
+
+  def total_rounds_played
+    self.rounds_played.length
   end
 
   def total_correct_on_first_try 
-    self.rounds.reduce(0){|sum, round| sum + round.correct_on_first_try.count}
+    self.rounds_played.reduce(0){|sum, round| sum + round.correct_on_first_try.count}
   end
 
   def total_guesses
-    self.rounds.reduce(0){|sum, round| sum + round.guesses.length}
+    self.rounds_played.reduce(0){|sum, round| sum + round.guesses.length}
   end
 
   def percent_correct_on_first_try
     if self.total_guesses != 0
-      "#{self.total_correct_on_first_try / (self.total_guesses.to_f * 100).round}%"
+      "#{((self.total_correct_on_first_try / self.total_guesses.to_f) * 100).round}%"
     else
       "N/A"
     end
