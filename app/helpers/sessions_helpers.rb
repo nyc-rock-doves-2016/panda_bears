8ffd81ab433cc11101e4a3189e70bdbf8c6e74f8
Daniel Homer <daniel.homer6@gmail.com>
2016-04-17 21:03:00 -0400

    Fix authentication spoofing issue

--- app/helpers/sessions_helpers.rb
+++ app/helpers/sessions_helpers.rb
@@ -1,6 +1,6 @@
 helpers do
 	def logged_in?
-		session[:user_id] && @user == User.find_by(id: session[:user_id])
+		session[:user_id] && User.find_by(id: session[:user_id])
 	end
 
 	def require_user
